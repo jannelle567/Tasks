1e7bf808ee1913b155fc3dec8d3178a7782a5d68
Jannelle O Andres <159749335+jannelle567@users.noreply.github.com>
2024-06-15 06:24:30 +0000

    done adding output

---
+++ LargetsNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.io.*;
+
+class AssignmentThree {
+    public static void main(String[] args) {
+        int uno = 0, dos = 0, tres = 0;
+    
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter 3 random number: ");
+    uno = scan.nextInt();
+    dos = scan.nextInt();
+    tres = scan.nextInt();
+    
+     if (uno == dos && uno == tres) {
+            System.out.println("All numbers are equal");
+}
+int largest;
+    if (uno >= dos && uno >= tres) {
+                largest = uno;
+            } else if (dos >= uno && dos >= tres) {
+                largest = dos;
+            } else {
+                largest = tres;
+            }
+            System.out.println("The largest number is " + largest);
+        }
+}
