47ab189c4af0694929d95c7ec5772429dcbb2a02
Jannelle O Andres <159749335+jannelle567@users.noreply.github.com>
2024-06-19 08:16:58 +0000

    done adding output

---
+++ main.java
@@ -0,0 +1,12 @@
+public class main {
+     public static void main(String[] args) {
+        int original1 = 1;
+        int original2 = 2;
+        swap(original1, original2);
+    System.out.println(original1);
+    System.out.println(original2);
+     }
+}
+public static void swap (int a, int b) {
+    int c = a; a = b; b = c;
+}   
